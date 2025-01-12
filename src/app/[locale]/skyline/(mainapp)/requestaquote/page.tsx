1ec46d5f377ae4db6a740e9b96df592af9c6140e
jirapatton <oottoonnoo@gmail.com>
2025-01-12 17:41:09 +0700

    Update page.tsx

--- src/app/[locale]/skyline/(mainapp)/requestaquote/page.tsx
+++ src/app/[locale]/skyline/(mainapp)/requestaquote/page.tsx
@@ -1,7 +1,13 @@
+import ContactForm from "@/app/[locale]/_components/ContactForm";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className=" flex justify-center p-20">
+      <h1 className="text-4xl font-bold">Request a Quote</h1>
+      <ContactForm />
+    </div>
+  );
 };
 
 export default page;
